63b43ddcc9f569cdace3679a091849d498539658
Shaq-Mg <shaqmcg0@gmail.com>
2024-05-14 12:49:06 +0100

    ClientDataController added

---
+++ CoreData/DataController/ClientController.swift
@@ -0,0 +1,57 @@
+//
+//  ClientController.swift
+//  JournalPro
+//
+//  Created by Shaquille McGregor on 14/05/2024.
+//
+
+import Foundation
+import CoreData
+
+class ClientController {
+    let container = NSPersistentContainer(name: "ClientModel")
+    
+    init() {
+        container.loadPersistentStores { desc, error in
+            if let error = error {
+                print("Failed to load the data \(error.localizedDescription)")
+            }
+        }
+    }
+    func save(context: NSManagedObjectContext) throws {
+        if context.hasChanges {
+            try context.save()
+            print("Data has saved successfully")
+        }
+    }
+    func addClient(firstName: String, lastName: String, phoneNumber: String,
+                   isFavorite: Bool, notes: String, context: NSManagedObjectContext) {
+        let client = Client(context: context)
+        client.id = UUID()
+        client.firstName = firstName
+        client.lastName = lastName
+        client.phoneNumber = phoneNumber
+        client.isFavourite = isFavorite
+        client.notes = notes
+        
+       try? save(context: context)
+    }
+    func editClient(client: Client, firstName: String, lastName: String, phoneNumber: String,
+                   isFavorite: Bool, notes: String, context: NSManagedObjectContext) {
+        client.firstName = firstName
+        client.lastName = lastName
+        client.phoneNumber = phoneNumber
+        client.isFavourite = isFavorite
+        client.notes = notes
+        
+       try? save(context: context)
+    }
+}
+//    func save(context: NSManagedObjectContext) {
+//        do {
+//            try context.save()
+//            print("Data has saved successfully")
+//        } catch {
+//            print("Unable to save context")
+//        }
+//    }
