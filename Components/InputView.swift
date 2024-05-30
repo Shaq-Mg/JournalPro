1e9c58a1a64ced7766f2e053458e150ee47ae1df
Shaq-Mg <shaqmcg0@gmail.com>
2024-05-30 14:05:43 +0100

    InputView added

---
+++ Components/InputView.swift
@@ -0,0 +1,48 @@
+//
+//  InputView.swift
+//  JournalPro
+//
+//  Created by Shaquille McGregor on 30/05/2024.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecureField = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundStyle(.secondary)
+                .font(.footnote)
+            
+            VStack {
+                if isSecureField {
+                    SecureField(placeholder, text: $text)
+                        .font(.system(size: 16))
+                    
+                } else {
+                    TextField(placeholder, text: $text)
+                        .font(.system(size: 16))
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.white)
+            .cornerRadius(20)
+            .overlay(RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 1).foregroundStyle(.secondary))
+            .shadow(radius: 5)
+        }
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView(text: .constant(""), title: "Preview", placeholder: "Preview")
+    }
+}
