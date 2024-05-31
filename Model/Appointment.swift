ef1ba03e3086653ca9e5c0274a1481ca0cc344d3
Shaq-Mg <shaqmcg0@gmail.com>
2024-05-31 20:15:20 +0100

    Apoointment, Service models added with FirebaseConstants

---
+++ Model/Appointment.swift
@@ -0,0 +1,22 @@
+//
+//  Appointment.swift
+//  JournalPro
+//
+//  Created by Shaquille McGregor on 31/05/2024.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Appointment: Identifiable, Codable {
+    @DocumentID var id: String?
+    let name: String
+    let service: Service
+    let date: Date
+    
+//    init(data: [String: Any]) {
+//        self.name = data[FirebaseConstants.service] as? String ?? "n/a"
+//        self.date = data["date"] as? Date ?? Date()
+//    }
+}
