68ff6eb1b334752fb91172d2f16b8bdfb3223592
Shaq-Mg <shaqmcg0@gmail.com>
2024-06-09 19:24:34 +0100

    AppointmentInputView created 😀

---
+++ Components/ApptInputView.swift
@@ -0,0 +1,39 @@
+//
+//  ApptInputView.swift
+//  JournalPro
+//
+//  Created by Shaquille McGregor on 09/06/2024.
+//
+
+import SwiftUI
+
+struct ApptInputView: View {
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundStyle(.black)
+                .font(.footnote)
+            
+            VStack {
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 16))
+                    .textInputAutocapitalization(.none)
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.white)
+            .cornerRadius(20)
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1).foregroundStyle(.black))
+        }
+    }
+}
+
+struct ApptInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        ApptInputView(text: .constant(""), title: "Name", placeholder: "Name")
+    }
+}
