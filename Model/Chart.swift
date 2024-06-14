d62d2f7d37e56e2abe4271c4d949bd819b2a7aca
Shaq-Mg <shaqmcg0@gmail.com>
2024-06-14 17:25:54 +0100

    Chart enum for ChartViewPicker

---
+++ Model/Chart.swift
@@ -0,0 +1,20 @@
+//
+//  Chart.swift
+//  JournalPro
+//
+//  Created by Shaquille McGregor on 14/06/2024.
+//
+
+import Foundation
+
+enum Chart: CaseIterable {
+case last7Days, next7Days
+    
+    var title: String {
+        switch self {
+        case .last7Days: return "Previous week"
+        case .next7Days: return "Next week"
+            
+        }
+    }
+}
