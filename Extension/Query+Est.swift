53635f2399ddc50404db139c01aac974ba234587
Shaq-Mg <shaqmcg0@gmail.com>
2024-06-14 13:04:50 +0100

    fetchHours functions with @escaping addSnapshotListener

---
+++ Extension/Query+Est.swift
@@ -0,0 +1,12 @@
+//
+//  Query+Est.swift
+//  JournalPro
+//
+//  Created by Shaquille McGregor on 14/06/2024.
+//
+
+import Foundation
+
+extension Query {
+
+}
--- ViewModel/CalenderViewModel.swift
+++ ViewModel/CalenderViewModel.swift
@@ -12,6 +12,7 @@ import FirebaseFirestoreSwift
 class CalenderViewModel: ObservableObject {
     @Published var availableDates = [Date]()
     @Published var availableDays: Set<String> = []
+    @Published var hours = [Hours]()
     @Published var selectedMonth = 0
     @Published var showConfirmedAppt = false
     @Published var currentDate: Date?
@@ -59,9 +60,37 @@ class CalenderViewModel: ObservableObject {
         return month!
     }
     
-    private func fetchHours() async throws -> [Hours] {
-        
-        return []
+//    private func fetchHours(completion: @escaping (_ hours: [Hours]) -> ()) {
+//        db.collection("hours").addSnapshotListener { (querySnapshot, error) in
+//            guard let documents = querySnapshot?.documents else {
+//                print("No documents")
+//                return
+//            }
+//            let hours: [Hours] = documents.compactMap({ try? $0.data(as: Hours.self) })
+//            completion(hours)
+//
+//            querySnapshot?.documentChanges.forEach({ change in
+//                if (change.type == .added) {
+//                    print("New hours: \(change.document.data())")
+//                }
+//                if (change.type == .modified) {
+//                    print("Modified hours: \(change.document.data())")
+//                }
+//                if (change.type == .removed) {
+//                    print("Removed hours: \(change.document.data())")
+//                }
+//            })
+//        }
+//    }
+    
+    private func fetchHours() {
+        db.collection("hours").addSnapshotListener { (querySnapshot, error) in
+            if let querySnapshot = querySnapshot {
+                self.hours = querySnapshot.documents.compactMap { document in
+                    try? document.data(as: Hours.self)
+                }
+            }
+        }
     }
     
     private func fetchAvailableAppointments() async throws -> [Date] {
@@ -70,30 +99,30 @@ class CalenderViewModel: ObservableObject {
     }
     
     private func generateAppointmentTimes(from appts: [Appointment]) async throws -> [Date] {
-        let takenAppts: Set<Date> = Set(appts.map({ $0.date }))
-        let hours = try await fetchHours()
-        let calender = Calendar.current
-        let currentWeekDay = calender.component(.weekday, from: Date()) - 2
-        
-        var timeSlots = [Date]()
-        for weekOffset in 0...4 {
-            let daysOffset = weekOffset * 7
-            
-            for hour in hours {
-                if hour.start != 0 && hour.end != 0 {
-                    var currentDate = calender.date(from: DateComponents(year: calender.component(.year, from: Date()), month: calender.component(.month, from: Date()), day: calender.component(.day, from: Date()) + (hour.day - currentWeekDay), hour: hour.start))
-                    
-                    while let nextDate = calender.date(byAdding: .minute, value: 15, to: currentDate ?? Date()),
-                          calender.component(.hour, from: nextDate) <= hour.end {
-                        
-                        if !takenAppts.contains(currentDate ?? Date()) && currentDate ?? Date() > Date() && calender.component(.hour, from: currentDate ?? Date()) != hour.end {
-                            timeSlots.append(currentDate ?? Date())
-                        }
-                        currentDate = nextDate
-                    }
-                }
-            }
-        }
+//        let takenAppts: Set<Date> = Set(appts.map({ $0.date }))
+//        let hours = try await fetchHours()
+//        let calender = Calendar.current
+//        let currentWeekDay = calender.component(.weekday, from: Date()) - 2
+//
+//        var timeSlots = [Date]()
+//        for weekOffset in 0...4 {
+//            let daysOffset = weekOffset * 7
+//
+//            for hour in hours {
+//                if hour.start != 0 && hour.end != 0 {
+//                    var currentDate = calender.date(from: DateComponents(year: calender.component(.year, from: Date()), month: calender.component(.month, from: Date()), day: calender.component(.day, from: Date()) + (hour.day - currentWeekDay), hour: hour.start))
+//
+//                    while let nextDate = calender.date(byAdding: .minute, value: 15, to: currentDate ?? Date()),
+//                          calender.component(.hour, from: nextDate) <= hour.end {
+//
+//                        if !takenAppts.contains(currentDate ?? Date()) && currentDate ?? Date() > Date() && calender.component(.hour, from: currentDate ?? Date()) != hour.end {
+//                            timeSlots.append(currentDate ?? Date())
+//                        }
+//                        currentDate = nextDate
+//                    }
+//                }
+//            }
+//        }
         return []
     }
     
