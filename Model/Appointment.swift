52921af47c3b6e708771aa808bdb963baaef7a4c
Shaq-Mg <shaqmcg0@gmail.com>
2024-06-25 13:13:16 +0100

    CodingKeys with encode & decode

--- Model/Appointment.swift
+++ Model/Appointment.swift
@@ -15,7 +15,25 @@ struct Appointment: Identifiable, Codable {
     let service: Service
     let date: Date
     
-    enum codingKeys: String, CodingKey {
-        case name, service, date
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case service = "sevice"
+        case date = "date"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self._id = try container.decode(DocumentID<String>.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.service = try container.decode(Service.self, forKey: .service)
+        self.date = try container.decode(Date.self, forKey: .date)
+    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self._id, forKey: .id)
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.service, forKey: .service)
+        try container.encode(self.date, forKey: .date)
     }
 }
--- Model/Service.swift
+++ Model/Service.swift
@@ -11,10 +11,30 @@ import FirebaseFirestoreSwift
 
 struct Service: Identifiable, Codable {
     @DocumentID var id: String?
-    let title, price: String
+    let title: String
+    let price: String
     let duration: String
-//    init(data: [String: Any]) {
-//        self.title = data[FirebaseConstants.title] as? String ?? "n/a"
-//        self.price = data[FirebaseConstants.price] as? String ?? "n/a"
-//    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case title = "title"
+        case price = "price"
+        case duration = "duration"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self._id = try container.decode(DocumentID<String>.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.price = try container.decode(String.self, forKey: .price)
+        self.duration = try container.decode(String.self, forKey: .duration)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self._id, forKey: .id)
+        try container.encode(self.title, forKey: .title)
+        try container.encode(self.price, forKey: .price)
+        try container.encode(self.duration, forKey: .duration)
+    }
 }
